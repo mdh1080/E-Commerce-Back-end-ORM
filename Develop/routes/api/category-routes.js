f6b0b1294ec88d2407d0917424100263af37bbe8
@mdh1080 <110183347+mdh1080@users.noreply.github.com>
2022-12-14 12:48:59 -0500

    Edit tag-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,44 +1,34 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
 
   Category.findAll({
     include:[Product]
   })
-
   .then(categories=>{res.json(categories)})
   .catch(err=>{res.json(err)})
 
 });
 
 router.get('/:id', (req, res) => {
-
   Category.findOne({
     where:{
       id:req.params.id
     },
     include:[Product]
   })
-
   .then(categories=>{res.json(categories)})
   .catch(err=>{res.json(err)})
-
-
 });
 
 router.post('/', (req, res) => {
-  // create a new category
   Category.create(req.body)
-
   .then(categories=>{res.json(categories)})
   .catch(err=>{res.json(err)})
 });
 
 router.put('/:id', (req, res) => {
-
   Category.update(req.body,{
     where:{
       id:req.params.id
@@ -46,11 +36,9 @@ router.put('/:id', (req, res) => {
   })
   .then(categories=>{res.json(categories)})
   .catch(err=>{res.json(err)})
-  // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
   Category.destroy({
     where:{
       id:req.params.id
